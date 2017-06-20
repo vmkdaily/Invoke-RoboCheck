7f8c19e9569473348392a28ca2b1a9cc32788feb
Mike Nisk <webmaster@vmkdaily.com>
2017-06-19 23:30:26 -0500

    Update Invoke-RoboCheck.psm1

--- Invoke-RoboCheck.psm1
+++ Invoke-RoboCheck.psm1
@@ -3,7 +3,7 @@
     Author:       	Mike Nisk 
     Organization: 	vmkdaily 
     Filename:     	Invoke-RoboCheck.psm1
-    Version:        2.0.0
+    Version:            2.0.0
     -------------------------------------------------------------------------
     Module Name: Invoke-RoboCheck
     ===========================================================================
@@ -28,63 +28,18 @@
     - Run the script (i.e.Invoke-RoboCheck -Computer vcenter01.lab.local)
     - Upon first run, $Env:USERPROFILE\Robo-Check.ini is created for you (modify if desired)
 
-
-    ## MODULE CONTENTS
-    Invoke-RoboCheck.psd1
-    Invoke-RoboCheck.psm1
-
     ## DEPENDS ON
     $ENV:USERPROFILE\Invoke-RoboCheck.ini
     *CREATED AT RUNTIME IF NEEDED
 
-    ## INSTRUCTIONS
-    Step 1.  Download and install PowerCLI
-             www.vmware.com/go/powercli
-             Note:  This module supports PowerCLI versions 5.0 to 6.5+.
-             PowerCLI 6.5 is preferred, but not required.
-
-    Step 2.  Windows update your machine and upgrade your Powershell version if possible.
-             This module supports Powershell versions 3 or greater (Powershell 5 preferred, not required).
-             You can check your Powershell version by running $PSVersionTable.
-             Note:  This module not intended for or tested on Powershell 6 for linux.
-
-    Step 3.  Execution Policy
-             From an elevated Powershell prompt run the following:
-	     
-             C:\> Get-ExecutionPolicy
-             Note: If it comes back as restricted, then you need to change it to RemoteSigned, Unrestricted or Bypass.
-             Set-ExecutionPolicy RemoteSigned -Confirm:$false
-
-    Step 4.  Download the module to the desired path.  You need the entire Invoke-RoboCheck folder.
-
-    Step 5.  After downloading, right-click the folder and go to properties.  Click on 'unblock' if needed.
-
-    Step 6.  Launch Powershell as administrator (i.e. right-click run as administrator)
-
-    Step 7.  Import the Module into your Powershell session by using Import-Module.
-             We do this by pointing to the folder.  You should choose a good location, but for this
-             example, we are just saving the module to c:\temp and importing from there.
-
-             C:\> Import-Module C:\temp\Invoke-RoboCheck -Verbose
-            VERBOSE: Loading module from path 'C:\temp\Invoke-RoboCheck\Invoke-RoboCheck.psd1'.
-            VERBOSE: Loading module from path 'C:\temp\Invoke-RoboCheck\Invoke-RoboCheck.psm1'.
-            VERBOSE: Importing function 'Invoke-RoboCheck'.
-             C:\>
-
-             Now that the above is done, you can start using the module by consuming it's main function,
-             known as Invoke-RoboCheck (same as the module name).
-
-             See the cmdlet help for more information
-             Get-Help Invoke-RoboCheck -ShowWindow
-             Get-Help Invoke-RoboCheck -Full
-             Get-Help Invoke-RoboCheck -Examples
-
-    Step 8.  The first time you run Invoke-RoboCheck, a file called Invoke-RoboCheck.ini is created for you.
-             This file controls the configuration for things like path to Credential files (optional), output
-             directories, naming conventions, etc.  This is stored in your user profile ($Env:USERPROFILE) to ensure
-             functionality for multiple users on the same box.
+    ## PREFERENCES
+    The first time you run Invoke-RoboCheck, a file called Invoke-RoboCheck.ini is created for you.
+    This file controls the configuration for things like path to Credential files (optional), output
+    directories, naming conventions, etc.  This is stored in your user profile ($Env:USERPROFILE) to ensure
+    functionality for multiple users on the same box.
 	     
        Note:  The paths in your config file will show full paths.  It will not actually show $env variables.
+       Do not use quotes when manually editing your config file.
 
               ##EXAMPLE OF DEFAULT CONFIG FILE
 
@@ -107,7 +62,6 @@
 
 # Function loader based on:
 # https://github.com/adamrushuk/PSvCloud/blob/master/PSvCloud/PSvCloud.psm1
-
 
 # Get public and private function files
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
